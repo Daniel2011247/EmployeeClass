a52a692ee74af0a5631eda21bcdcdb0819353bee
Daniel Fakolujo <folawemifakolujo@gmail.com>
2020-07-07 12:36:53 +0100

    Second commit

---
+++ EmployeeTest.java
@@ -0,0 +1,15 @@
+package codes;
+import java.util.Scanner;
+public class EmployeeTest {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter FirstName");
+		String firstName = sc.next();
+		System.out.println("Enter LastName");
+		String lastName = sc.next();
+		System.out.println("Enter MonthlySalary");
+		double monthlySalary = sc.nextDouble();
+		
+		
+	}
+}
